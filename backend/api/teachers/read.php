87f66e03d52772c08fa319293315a77e24284926
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-02-12 10:18:58 -0500

    finished first draft of API, debugging now

--- backend/api/teachers/read.php
+++ backend/api/teachers/read.php
@@ -1,3 +1,47 @@
 <?php
-    $serverName = "KAZDESKTOP";
-    $databaseName = "lbpsb_project"; 
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../Database.php';
+    include_once '../../models/Teacher.php';
+
+    // Instantiate Database object & connect to it
+    $database = new Database();
+    $pdo = $database->connect();
+
+    // Instantiate Teacher object
+    $teacher = new Teacher($database);
+
+    // Teacher query
+    $result = $teacher->read();
+
+    // Get row count
+    $num = $result->rowCount();
+
+    // Check if any teachers
+    if ($num > 0) {
+        $teachers_arr = [];
+        $teachers_arr['data'] = [];
+
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+
+            $teacher_item = [
+                'id' => $id,
+                'subject' => $subject,
+                'grade' => $grade,
+                'firstName' => $firstName,
+                'lastName' => $lastName,
+                'school' => $school
+            ];
+
+            // Push to 'data'
+            array_push($teachers_arr['data'], $teacher_item);
+        }
+
+        // Turn to JSON
+        echo json_encode($teachers_arr);
+    } else {
+        echo json_encode(['message' => 'No Teachers Found']);
+    }
